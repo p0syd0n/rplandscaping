a10abcb76f5bf3f9cb11003966d873bcb0a2c9f1
p0syd0n <ajeff9752@gmail.com>
2025-08-28 14:07:35 +0000

    Everything work 2

--- index.js
+++ index.js
@@ -38,8 +38,8 @@ function deleteEntry(id) {
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 app.use(express.static(path.join(__dirname, "public")));
-app.use(express.json()); // Middleware to parse JSON bodies
-app.use(express.urlencoded({ extended: true })); // Middleware to parse URL-encoded bodies
+app.use(express.json()); 
+app.use(express.urlencoded({ extended: true }));
 
 
 app.get("/", (req, res) => {
